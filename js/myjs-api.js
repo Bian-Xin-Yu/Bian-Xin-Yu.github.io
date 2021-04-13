322165ad92d2b7e694de3b266835e014fc561f3c
Bian-Xin-Yu <baby@bianyu.xin>
2021-04-13 16:08:28 +0800

    可JS调用的百度收录推送API-替代百度自动推送v2版部署前准备

--- js/myjs-api.js
+++
@@ -1,6 +0,0 @@
-var curWwwPath = window.document.location.href;
-var keyValue = "thhoH0JaP6xN9GLF";//填写推送用的准入密钥
-var apiValue = "https://api.uosblog.top/api.php";//api地址
-valueValue = apiValue + "?url=" + curWwwPath + "&key=" + keyValue;
-document.write('<div class="apiheader"><p>百度收录:</p><img id="myImage" src="" width="20%"></div>');
-document.getElementById("myImage").src = valueValue;
